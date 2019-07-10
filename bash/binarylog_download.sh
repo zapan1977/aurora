dc97b977ab346f89c273ce03c0f7055074c911f0
Soohyun Park <cdnw.monyog.alert@gmail.com>
2019-07-10 11:46:05 +0900

    Update binarylog_download.sh

--- bash/binarylog_download.sh
+++ bash/binarylog_download.sh
@@ -1,14 +1,16 @@
 # Amazon RDS Binary Log 다운로드. mysqlbinlog Util을 사용해서 원격에서 Download.
-# 
+# --login-path=name 지원 안함.
 mysqlbinlog \
-    --read-from-remote-server \
-    --host=pfproductv2.cqhvl55yvzxw.ap-northeast-2.rds.amazonaws.com \
-    --port=3306  \
-    --user <User> \
-    --password \
-    --raw \
-    --result-file=/tmp/ \
-    binlog.00098
+--read-from-remote-server \
+--host=<MySQL Endpoint or Host> \
+--port=3306  \
+--user <User> \
+--password \
+--raw \
+--result-file=<Local Path>/<binary log file name>
 
-# 쿼리 확인을 위한 옵션
-mysqlbinlog -vv --base64-output=DECODE-ROWS logfile
+# Binary Log statement 파일로 변환.
+mysqlbinlog \
+-vv \
+--base64-output=DECODE-ROWS \
+<binary log file> >> file_name.sql or file_name.txt
