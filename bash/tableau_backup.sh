7b252f2d6740040e2fce43ffc5936affd05d2a8a
Soohyun Park <cdnw.monyog.alert@gmail.com>
2019-07-11 10:04:48 +0900

    TSM Login/Logout add.
    타블로 백업을 위해서 User login 이 필요.
    백업 전 Login / 백업 후 Logout.

--- bash/tableau_backup.sh
+++ bash/tableau_backup.sh
@@ -9,13 +9,23 @@ BACKUP_DATE=`date +"%Y%m%d"`
 #백업파일을 저장할 경로
 BACKUP_DIR=/home/peoplefund/tableau-server-backup
 
+# Tableua information
+Tableau_USER=""
+Tableau_PWD=""
+Tableau_TSM="/opt/tableau/tableau_server/packages/customer-bin.20181.18.0404.1605/tsm"
+
+# Tableua Login
+${Tableau_TSM} login --username ${Tableau_USER} --password ${Tableau_PWD}
+
 # Tableua 서버 토폴로지 및 구성 데이터 백업
-/opt/tableau/tableau_server/packages/customer-bin.20181.18.0404.1605/tsm \
-settings export -f ${BACKUP_DIR}/pf_tableau_settings_${BACKUP_DATE}.json
+${Tableau_TSM} settings export -f ${BACKUP_DIR}/pf_tableau_settings_${BACKUP_DATE}.json
 
 # Tableua 리포지토리 데이터 백업
-/opt/tableau/tableau_server/packages/customer-bin.20181.18.0404.1605/tsm \
-maintenance backup -f pf_tableau_repo_${BACKUP_DATE}.tsbak
+${Tableau_TSM} maintenance backup -f pf_tableau_repo_${BACKUP_DATE}.tsbak
+
+# Tableua session logout
+${Tableau_TSM} logout --username ${Tableau_USER} --password ${Tableau_PWD}
+
 # Backup file 이동
 mv /var/opt/tableau/tableau_server/data/tabsvc/files/backups/pf_tableau_repo_${BACKUP_DATE}.tsbak \
 ${BACKUP_DIR}/pf_tableau_repo_${BACKUP_DATE}.tsbak
@@ -25,7 +35,7 @@ find ${BACKUP_DIR}/ -mtime +3 -exec rm -f {} \;
 
 # 완료시각
 END_DATE=`date`
-echo "시작: ${NOW_DATE} 완료: ${END_DATE}"
+echo "시작: ${NOW_DATE} 완료: ${END_DATE}" 
 
 ## Crontab
 # 1 1 * * * /home/peoplefund/scripts/tableau-backup.sh >> /home/peoplefund/scripts/tableau-backup.txt 2>&1
